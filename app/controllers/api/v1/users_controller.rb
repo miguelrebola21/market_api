7133b76772bba7c3552fa4a7c0096a6cfd461689
Miguel Rebola <miguelrebola21@gmail.com>
2015-10-19 12:13:49 +0100

    added index method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::UsersController < ApplicationController
   respond_to :json
-
+  def index
+  	raise
+  end	
   def show
     respond_with User.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 require 'api_constraints'
 Rails.application.routes.draw do
   devise_for :users
- namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do
+ namespace :api, defaults: { format: :json }, path: '/'  do
   scope module: :v1,
               constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
-      resources :users, :only => [:show]
+      resources :users, :only => [:show,:index]
     end
  end 
 end
